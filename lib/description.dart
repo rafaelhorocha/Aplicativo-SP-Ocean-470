66ed461cb328b07f9789eab71c4cf2213577719f
Rafael Rocha <rafinhaorocha21@gmail.com>
2024-06-17 00:00:12 -0300

    second commit

--- lib/description.dart
+++ lib/description.dart
@@ -1,4 +1,6 @@
-import 'package:flutter/material.dart';
+// ignore_for_file: file_names
+
+import 'package:flutter/material.dart'; // For PNG icons (if using Image.asset)
 
 void main() {
   runApp(const SPOceanApp());
@@ -36,42 +38,179 @@ class _SPOceanHomePageState extends State<SPOceanHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      // Cor de fundo da tela
-      backgroundColor: const Color(0xFFE9F9FF),
-      appBar: AppBar(
-        // Centraliza o título no AppBar
-        centerTitle: true,
-        title: const Text(
-          'SP OCEAN',
-          style: TextStyle(
-            color: Color(0xFF005374),
-            fontSize: 40,
-            fontWeight: FontWeight.bold,
+      backgroundColor: const Color.fromRGBO(233, 249, 255, 1),
+
+      // Wrap the body with Scrollbar
+      body: Scrollbar(
+        child: SingleChildScrollView(
+          // Rest of your content goes here
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              // Image with full width and fixed height
+              Image.asset(
+                'assets/img/snorkels.png',
+                width: double.infinity, // Occupy full width
+                height: 300, // Fixed height
+                fit: BoxFit.cover, // Adjust fit as needed
+              ),
+
+              // Row for icon and text alignment
+              Row(
+                mainAxisAlignment:
+                    MainAxisAlignment.center, // Center the content
+                children: [
+                  // Replace icon with image
+                  // Choose the desired icon
+                  const SizedBox(width: 10), // Add some horizontal spacing
+
+                  const Text(
+                    'Snorkel',
+                    style: TextStyle(
+                      color: Color(0xFF005374),
+                      fontSize: 30,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Image.asset(
+                    'assets/img/icons.png', // Replace with your image filename
+                    width: 80.0, // Adjust image width as desired
+                    height: 80.0, // Adjust image height as desired
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ), // Espaço entre o AppBar e o texto "Atividades"
+                ],
+              ),
+
+              const Padding(
+                padding: EdgeInsets.only(top: 0, left: 25, right: 25.0),
+                child: Text(
+                  'Mergulho com snorkel é nadar perto da superfície da água usando um tubo chamado snorkel para respirar. Você pode ver embaixo d\'água sem precisar segurar a respiração.',
+                  style: TextStyle(
+                    color: Color.fromRGBO(10, 116, 158, 1),
+                    fontSize: 18,
+                  ),
+                  textAlign: TextAlign.justify, // Justify text alignment
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              const Padding(
+                padding: EdgeInsets.fromLTRB(25, 10.0, 140.0, 0.0),
+                //padding: EdgeInsets.only(top: 20, left: 0, right: 140),
+                child: Text(
+                  'Praias com essa atividade:',
+                  style: TextStyle(
+                    color: Color(0xFF005374),
+                    fontSize: 28,
+                    fontWeight: FontWeight.bold,
+                  ),
+                  textAlign: TextAlign.left, // Justify text alignment
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+
+              // Three stacked containers
+
+              Column(
+                children: [
+                  Container(
+                    decoration: BoxDecoration(
+                      color: const Color.fromARGB(255, 197, 237, 252),
+                      borderRadius: BorderRadius.circular(50.0),
+                    ),
+                    padding: const EdgeInsets.all(10.0), // Add padding
+                    margin: const EdgeInsets.fromLTRB(
+                        30, 10.0, 260.0, 0.0), // Add margin
+
+                    child: Row(
+                      children: [
+                        // Replace with your desired image
+                        Image.asset(
+                          'assets/img/sol.png', // Replace with your image filename
+                          width: 30.0, // Adjust image width as desired
+                          height: 30.0, // Adjust image height as desired
+                        ),
+                        const SizedBox(height: 0.0),
+                        const Text(
+                          'Praia Prumirim',
+                          style: TextStyle(
+                            color: Color.fromRGBO(10, 116, 158, 1),
+                            fontSize: 20.0,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  const SizedBox(height: 10.0),
+                  Container(
+                    decoration: BoxDecoration(
+                      color: const Color.fromARGB(255, 197, 237, 252),
+                      borderRadius: BorderRadius.circular(50.0),
+                    ),
+                    padding: const EdgeInsets.all(10.0), // Add padding
+                    margin: const EdgeInsets.fromLTRB(
+                        30, 10.0, 260.0, 0.0), // Add margin
+
+                    child: Row(
+                      children: [
+                        // Replace with your desired image
+                        Image.asset(
+                          'assets/img/sol.png', // Replace with your image filename
+                          width: 30.0, // Adjust image width as desired
+                          height: 30.0, // Adjust image height as desired
+                        ),
+                        const SizedBox(height: 10.0),
+                        const Text(
+                          'Praia Mongaguá',
+                          style: TextStyle(
+                            color: Color.fromRGBO(10, 116, 158, 1),
+                            fontSize: 20.0,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  const SizedBox(height: 10.0),
+                  Container(
+                    decoration: BoxDecoration(
+                      color: const Color.fromARGB(255, 197, 237, 252),
+                      borderRadius: BorderRadius.circular(50.0),
+                    ),
+                    padding: const EdgeInsets.all(10.0), // Add padding
+                    margin: const EdgeInsets.fromLTRB(
+                        30, 10.0, 260.0, 0.0), // Add margin
+
+                    child: Row(
+                      children: [
+                        // Replace with your desired image
+                        Image.asset(
+                          'assets/img/sol.png', // Replace with your image filename
+                          width: 30.0, // Adjust image width as desired
+                          height: 30.0, // Adjust image height as desired
+                        ),
+                        const SizedBox(height: 0.0),
+                        const Text(
+                          'Praia Vermelha',
+                          style: TextStyle(
+                            color: Color.fromRGBO(10, 116, 158, 1),
+                            fontSize: 20.0,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  const SizedBox(height: 20.0),
+                ],
+              ),
+            ],
           ),
         ),
-        backgroundColor: Colors.transparent, // Remove o fundo do AppBar
       ),
-
-      body: const Padding(
-        padding: EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment:
-              CrossAxisAlignment.start, // Alinha os filhos à esquerda
-          children: <Widget>[
-            SizedBox(
-                height: 24), // Espaço entre o AppBar e o texto "Atividades"
-            Text(
-              'Atividades',
-              style: TextStyle(
-                color: Color(0xFF005374),
-                fontSize: 24, // Tamanho da fonte do subtítulo
-              ),
-            ),
-            // Aqui você pode adicionar os widgets para as atividades conforme necessário.
-          ],
-        ),
-      ),
-
       bottomNavigationBar: BottomNavigationBar(
         items: const <BottomNavigationBarItem>[
           BottomNavigationBarItem(
@@ -90,6 +229,7 @@ class _SPOceanHomePageState extends State<SPOceanHomePage> {
         currentIndex: _selectedIndex,
         selectedItemColor: Colors.white,
         unselectedItemColor: Colors.white70,
+
         backgroundColor:
             const Color(0xFF005374), // Cor de fundo do BottomNavigationBar
         onTap: _onItemTapped,
